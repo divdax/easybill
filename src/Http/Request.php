5f9cfc795973e22fea6eff3308566c975dd7b99f
Heiko Klingele <divdax@gmail.com>
2019-09-03 19:16:16 +0000

    Apply fixes from StyleCI

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -13,22 +13,26 @@ class Request
     {
         $this->client = new Client([
             'base_uri' => $api_endpoint,
-            'headers' => [
-                'Authorization' => 'Bearer ' . $api_key
-            ]
+            'headers'  => [
+                'Authorization' => 'Bearer ' . $api_key,
+            ],
         ]);
     }
 
     /**
      * intercept response of all requests.
      * parse json response
+     *
      * @param ResponseInterface $response
+     *
      * @return mixed|ResponseInterface
      */
-    protected function interceptResponse($response) {
-        if($response->getHeader('Content-Type')[0] === 'application/json') {
+    protected function interceptResponse($response)
+    {
+        if ($response->getHeader('Content-Type')[0] === 'application/json') {
             return json_decode($response->getBody());
         }
+
         return $response;
     }
 
@@ -39,32 +43,37 @@ class Request
             : '';
 
         $response = $this->client->get($url . $params);
+
         return $this->interceptResponse($response);
     }
 
     public function post($url, $attributes = null)
     {
         $response = $this->client->post($url, $attributes);
+
         return $this->interceptResponse($response);
     }
 
     public function put($url, $attributes = null)
     {
         $response = $this->client->put($url, $attributes);
+
         return $this->interceptResponse($response);
     }
 
     public function delete($url, $attributes = null)
     {
         $response = $this->client->delete($url, $attributes);
+
         return $this->interceptResponse($response);
     }
 
     private function build_url_query($url)
     {
-        if(is_array($url)) {
+        if (is_array($url)) {
             $url = http_build_query($url);
         }
+
         return trim($url, '/');
     }
 }
